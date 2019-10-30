13c05f5a455087e4f712ac5a34372aa7af73e358
Shin'ya UEOKA <ueokande@i-beam.org>
2019-10-30 03:39:44 +0000

    Build TypeScript files

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'build');
+
+config = {
+  entry: {
+    content: path.join(src, 'content'),
+    settings: path.join(src, 'settings'),
+    background: path.join(src, 'background'),
+  },
+
+  output: {
+    path: dist,
+    filename: '[name].js'
+  },
+
+  optimization: {
+    minimize: false
+  },
+
+  performance: {
+    hints: false
+  },
+
+  module: {
+    rules: [
+      {
+        test: [ /\.ts$/, /\.tsx$/],
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader?sourceMap=true'
+      },
+    ]
+  },
+
+  resolve: {
+    extensions: [ '.js', '.jsx', '.ts', '.tsx' ],
+    modules: [path.join(__dirname, 'src'), 'node_modules']
+  },
+};
+
+module.exports = config
